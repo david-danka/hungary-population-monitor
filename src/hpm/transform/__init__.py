ffbbbd728a9802dcec6082eea39d691ccc3174e3
David Danka <david.danka@proton.me>
2026-06-23 09:34:15 +0200

    feat(transform): expose public APIs via __init__

---
+++ src/hpm/transform/__init__.py
@@ -0,0 +1,9 @@
+from hpm.transform.population import transform_population_datasets
+from hpm.transform.settlement import transform_settlement_data
+from hpm.transform.star_schema import build_star_schema
+
+__all__ = [
+    "transform_population_datasets",
+    "transform_settlement_data",
+    "build_star_schema",
+]
